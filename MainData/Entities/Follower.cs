d3e909a8b926ca6ec51f93ae007f7abf8f7670da
Truong Giang <giangntse150747@gmail.com>
2023-06-28 12:59:46 +0700

    add follower table

---
+++ MainData/Entities/Follower.cs
@@ -0,0 +1,29 @@
+﻿using AppCore.Data;
+using AppCore.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MainData.Entities;
+
+public class Follower : BaseEntity
+{
+  public Guid FollowTo { get; set; }
+
+  //Relationship
+  public virtual User? User { get; set; }
+}
+
+public class FollowerConfig : IEntityTypeConfiguration<Follower>
+{
+  public void Configure(EntityTypeBuilder<Follower> builder)
+  {
+    builder.Property(x => x.FollowTo).IsRequired();
+
+    //Relationship
+    builder.HasOne(x => x.User)
+      .WithMany(x => x.Followers)
+      .HasForeignKey(x => x.FollowTo);
+
+  }
+}
